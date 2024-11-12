4141908d845543c4a4440a796e6ee7cfb29eee27
BnhbTk <53897578+BnhbTk@users.noreply.github.com>
2024-11-13 00:00:16 +0100

    Create chess.py

---
+++ chapter4/chess.py
@@ -0,0 +1,10 @@
+if __name__=="__main__":
+    # the outer loop is used to print rows
+    for row in range(8):
+        # the inner loop is used to print all the boxes in one row
+        for col in range(8):
+            # here we used a condition expression. The parameter end="" means that print insert no char after printing
+            # [ ] or [#]. By default, this parameters is \n (try to remove end="" to see the impact).
+            print("[ ]" if col%2==row%2 else "[#]",end="")
+        # print without any parameters creates a new line
+        print()
