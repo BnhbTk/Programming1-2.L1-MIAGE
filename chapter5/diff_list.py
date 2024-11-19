80e9778fbb53b35e03fb539937ac1ac63548eb28
BnhbTk <53897578+BnhbTk@users.noreply.github.com>
2024-11-19 01:44:42 +0100

    Create diff_list.py

---
+++ chapter5/diff_list.py
@@ -0,0 +1,14 @@
+if __name__=="__main__":
+    arr1=[1,2,3,4,5]
+    arr2=[1,2,3,6,5]
+
+    # let's use a flag to know whether the elements are the same
+    find=False
+    for i in range(len(arr1)):
+        if arr1[i]!=arr2[i]:
+            print("Difference at position",i)
+            find=True
+            break
+    # if all the elements are the same, find equals false
+    if not find:
+        print("These lists are the same")
