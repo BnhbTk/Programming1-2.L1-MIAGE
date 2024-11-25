a5cbd61d9c1a4deef840ba9e637f88bed1928c00
BnhbTk <53897578+BnhbTk@users.noreply.github.com>
2024-11-25 19:24:35 +0100

    Update fill_array.py

--- chapter5/fill_array.py
+++ chapter5/fill_array.py
@@ -1,9 +1,11 @@
 if __name__=="__main__":
-    arr=[0]*10
+    arr:list[int]=[0]*10
     # this loops fills the array with multiples of 3
+    i:int
     for i in range(len(arr)):
         arr[i]=3*i
     
     # display both indices and elements
+    e:int
     for i,e in enumerate(arr):
         print("Element at",i,"is",e)
