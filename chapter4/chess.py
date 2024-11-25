22ed61b781e3b082bf13b7315521d128c8da0b9d
BnhbTk <53897578+BnhbTk@users.noreply.github.com>
2024-11-25 19:46:59 +0100

    Update chess.py

--- chapter4/chess.py
+++ chapter4/chess.py
@@ -1,7 +1,9 @@
 if __name__=="__main__":
     # the outer loop is used to print rows
+    row:int
     for row in range(8):
         # the inner loop is used to print all the boxes in one row
+        col:int
         for col in range(8):
             # here we used a condition expression. The parameter end="" means that print insert no char after printing
             # [ ] or [#]. By default, this parameters is \n (try to remove end="" to see the impact).
