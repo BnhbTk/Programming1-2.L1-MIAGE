b6cf2b086266d38c9e6136e2154a07f72671d134
BnhbTk <53897578+BnhbTk@users.noreply.github.com>
2024-11-25 19:41:04 +0100

    Update reverse_1.py

--- chapter5/reverse_1.py
+++ chapter5/reverse_1.py
@@ -1,11 +1,12 @@
 if __name__=="__main__":
     # the array to be reversed
-    arr=[1,3,6,10,15,21]
+    arr:list[int]=[1,3,6,10,15,21]
 
     # Here, we're using another list to result
-    res=[0]*len(arr)
+    res:list[int]=[0]*len(arr)
 
     # in Python, -1 is used to access the last element, -2 the one before, and so on
+    i:int
     for i in range(len(arr)):
         res[i]=arr[-i-1]
     
