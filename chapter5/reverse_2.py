a910f54379438ddd3faf190819e5a051f7f5040d
BnhbTk <53897578+BnhbTk@users.noreply.github.com>
2024-11-25 19:41:45 +0100

    Update reverse_2.py

--- chapter5/reverse_2.py
+++ chapter5/reverse_2.py
@@ -1,11 +1,11 @@
 if __name__=="__main__":
     # the array to be reversed
-    arr=[1,3,6,10,15,21]
+    arr:list[int]=[1,3,6,10,15,21]
 
     # In this solution, we will use the same array to compute the reverse. We will have to browse half of the array an 'swap' it with the other half
 
-    i=0
-    j=len(arr)-1 # the index of the last element
+    i:int=0
+    j:int=len(arr)-1 # the index of the last element
     while i<j:
         arr[i],arr[j]=arr[j],arr[i]
         # Don't forget to update indices, otherwise the loop won't stop
