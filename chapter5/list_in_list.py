88d68144d0e1dae4fea1b4f9ae23a1ad0126e4b6
BnhbTk <53897578+BnhbTk@users.noreply.github.com>
2024-11-25 19:13:55 +0100

    Create list_in_list.py

---
+++ chapter5/list_in_list.py
@@ -0,0 +1,30 @@
+if __name__=="__main__":
+    # We check if arr1 occurs in arr2 and at which position
+    arr1=[1,5,4]
+    arr2=[4,1,5,1,5,4]
+    
+    # Since we don't know the position, initialize it to -1
+    position=-1
+
+    # loop until the length of arr2 - length of arr1
+    for i in range(len(arr2)-len(arr1)+1):
+        # if the first element is the same as the current position in arr2
+        if arr1[0]==arr2[i]:
+            find_diff=False
+            # test if arr1 is the same as the next len(arr1) elements of arr2
+            for j in range(1,len(arr1)):
+                if arr1[j]!=arr2[i+j]:
+                    find_diff=True
+                    break
+            # if no difference, we found a match
+            if not find_diff:
+                position=i
+                break
+    
+    # if position==-1 then no match has been found
+    if position>=0:
+        print(f"Occurrence found at position {position}")
+    else:
+        print("No occurrence")
+
+
